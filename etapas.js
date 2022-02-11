3bfeb1e8291212eac2309f0cae0815e331192cf9
Junior Gutierres <gutierres7j@outlook.com>
2022-02-11 20:01:52 -0300

    Envio de projeto

---
+++ etapas.js
@@ -0,0 +1,47 @@
+let etapas = [
+  {
+    titulo: "VEREADOR",
+    numeros: 5,
+    candidatos: [
+      {
+        numero: "10000",
+        nome: "Maria",
+        partido: "ABC",
+        fotos: [{ url: "candidato1.jpg", legenda: "Vereador" }],
+      },
+      {
+        numero: "20000",
+        nome: "Ricardo",
+        partido: "XYZ",
+        fotos: [{ url: "candidato2.jpg", legenda: "Vereador" }],
+      },
+    ],
+  },
+
+  {
+    titulo: "PREFEITO",
+    numeros: 2,
+    candidatos: [
+      {
+        numero: "10",
+        nome: "Ana",
+        partido: "ABC",
+        vice: "Gabriel",
+        fotos: [
+          { url: "candidato3.jpg", legenda: "Prefeito" },
+          { url: "candidato4.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+      {
+        numero: "20",
+        nome: "Rafael",
+        partido: "XYZ",
+        vice: "Hergesel",
+        fotos: [
+          { url: "candidato5.jpg", legenda: "Prefeito" },
+          { url: "candidato6.jpg", legenda: "Vice-Prefeito", small: true },
+        ],
+      },
+    ],
+  },
+];
